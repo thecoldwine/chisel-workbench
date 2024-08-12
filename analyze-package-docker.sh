cf2bebb3dd839bf0e65999bb55b32575f910c26b
Mikhail Aksenov <mikhail.aksenov@zaptec.com>
2024-08-12 13:23:55 +0000

    feat: add linting script + enhancements

--- analyze-package-docker.sh
+++ analyze-package-docker.sh
@@ -2,7 +2,7 @@
 
 CONTAINER_NAME=workbench
 
-while getopts "hc" flag; do
+while getopts "hct" flag; do
 	case $flag in
 		h) 
 			echo "Usage:  analyze-package-docker.sh [-ch] PACKAGE"
@@ -10,12 +10,16 @@ while getopts "hc" flag; do
 			echo "    PACKAGE - an ubuntu package to analyze"
 			echo "    -c - cleanup cached container"
 			echo "    -h - display this message"
-			return 0
+			echo "    -t - print package dependency tree"
+			exit 0
 			;;
 		c)
 			echo "Cleanup requested for container $CONTAINER_NAME"
 			docker container rm -f $CONTAINER_NAME
 			;;
+		t)	shift
+			docker exec $CONTAINER_NAME debtree $1
+			exit 0
 	esac
 done;
 
@@ -26,6 +30,7 @@ if [[ $? -ne 0 ]]; then
 	docker run -d --name $CONTAINER_NAME ubuntu:24.04 tail -f /dev/null
 	docker cp ./analyze-package.sh $CONTAINER_NAME:/opt/analyze-package.sh
 	docker exec $CONTAINER_NAME apt update
+	docker exec $CONTAINER_NAME apt install -y debtree
 fi
 
 docker exec $CONTAINER_NAME /opt/analyze-package.sh $1
---
+++ lint.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -e
+
+yamllint -c "./config.yaml" $HOME/Development/chisel-releases/slices/
+
+export LC_COLLATE=C
+EXIT_CODE=0
+
+err() {
+  echo "error:   " "$@" >&2
+  EXIT_CODE=1
+}
+warn() {
+  echo "warning: " "$@" >/dev/null
+}
+
+for filename in $(find $HOME/Development/chisel-releases/slices/ | grep "\.yaml$" | sort); do
+  yq '.slices | keys | .[]' "$filename" | sort -C || \
+    warn "$filename: slice names are not sorted"
+  for slice in $(yq '.slices | keys | .[]' "$filename"); do
+    key="$slice" yq \
+      '.slices | with_entries(select(.key == env(key))) | .[].essential[]' \
+      "$filename" | sort -C || \
+      err "$filename: $slice: \"essential\" entries are not sorted"
+    key="$slice" yq \
+      '.slices | with_entries(select(.key == env(key))) | .[].contents | select(.) | keys | .[]' \
+      "$filename" | sort -C || \
+      err "$filename: $slice: \"contents\" entries are not sorted"
+  done
+done
+
+exit $EXIT_CODE
